c14d93917587aa26b7ce33df3d65aac90d31d4ea
Ronald Rylands <z00d@freewillsociety.net>
2024-09-03 21:40:07 -0400

    Added worker script

---
+++ rke2-worker-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+FQDN="$1"
+TOKEN="$2"
+
+echo "IP entered: $FQDN"
+echo "Token entered: $TOKEN"
+
+systemctl disable --now ufw
+apt update
+apt install nfs-common -y open iscsi
+apt upgrade -y
+apt autoremove -y
+
+curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE=agent sh -
+mkdir -p /etc/rancher/rke2/
+echo "server: $FQDN:9345" >> /etc/rancher/rke2/config.yaml
+echo "token: $TOKEN" >> /etc/rancher/rke2/config.yaml
+systemctl enable rke2-agent.service
+systemctl start rke2-agent.service
+systemctl status rke2-agent.service
+
+
