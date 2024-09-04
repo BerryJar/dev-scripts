f7d1c8ac85ab79026bf5e9446afa60be33fdecb1
Ronald Rylands <z00d@freewillsociety.net>
2024-09-03 21:01:31 -0400

    Fixed order of commands.

--- rke2-control-install.sh
+++ rke2-control-install.sh
@@ -15,7 +15,6 @@ curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE=server sh -
 
 systemctl enable rke2-server.service
 systemctl start rke2-server.service
-systemctl status rke2-server.service
 
 ln -s $(find /var/lib/rancher/rke2/data/ -name kubectl) /usr/local/bin/kubectl
 export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
